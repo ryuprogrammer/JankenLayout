1cea4c2a8e97be6aa1ff4420b2d78763fd660565
トム・クルーズ <ryumagic@tomukuruzunoMacBook-Pro.local>
2023-03-18 20:21:44 +0900

    選択画面とゲーム画面のレイアウト作成

---
+++ JankenLayout/JankenLayout/JankenLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenLayoutApp.swift
+//  JankenLayout
+//
+//  Created by トム・クルーズ on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct JankenLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StageView()
+        }
+    }
+}
---
+++ JankenLayout/JankenLayout/animationView.swift
@@ -0,0 +1,20 @@
+//
+//  animationView.swift
+//  JankenLayout
+//
+//  Created by トム・クルーズ on 2023/03/18.
+//
+
+import SwiftUI
+
+struct animationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct animationView_Previews: PreviewProvider {
+    static var previews: some View {
+        animationView()
+    }
+}
